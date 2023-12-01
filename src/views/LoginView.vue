f3d10fdc84d3a6cabf42d12fd8f6b5cc5d8c2202
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-01 16:20:41 +0200

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+
+    <div class="input-group flex-nowrap">
+      <input type="text" class="form-control" placeholder="Kasutaja">
+    </div>
+
+    <div class="input-group flex-nowrap">
+      <input type="text" class="form-control" placeholder="Parool">
+    </div>
+
+
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'ErrorView'
+}
+
+</script>
+
