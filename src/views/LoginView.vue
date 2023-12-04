874aac581d7de681bc40d40562042ab7e9e7b100
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-04 11:46:02 +0200

    MAAS-4 wip

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,34 @@
 <template>
 
-  <div>
+
+  <div class="container text-center">
 
 
-    <div class="input-group flex-nowrap">
-      <input v-model="username" type="text" class="form-control" placeholder="Kasutaja">
-    </div>
 
-    <div class="input-group flex-nowrap">
-      <input v-model="password" type="text" class="form-control" placeholder="Parool">
-    </div>
+      <div class="row justify-content-center row-position">
 
-    <button @click=login type="submit" class="btn btn-outline-primary">Logi sisse</button>
+        <div class="col col-2">
+          <div class="col mb-2">
+            <input v-model="username" type="text" class="form-control" placeholder="Kasutaja">
+          </div>
+
+          <div class="col mb-2">
+            <input v-model="password" type="text" class="form-control" placeholder="Parool">
+          </div>
+          <button @click=login type="submit" class="btn btn-outline-primary">Logi sisse</button>
+        </div>
+
+      </div>
+
+
 
   </div>
+
 
 </template>
 
 
 <script>
-
 
 
 </script>
