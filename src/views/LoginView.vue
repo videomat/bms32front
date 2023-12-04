615fafe6a9e3f3fc714ba024971b6803e25f5ac1
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-04 10:23:45 +0200

    MAAS-4 wip

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,26 @@
 <template>
+
   <div>
 
 
     <div class="input-group flex-nowrap">
-      <input type="text" class="form-control" placeholder="Kasutaja">
+      <input v-model="username" type="text" class="form-control" placeholder="Kasutaja">
     </div>
 
     <div class="input-group flex-nowrap">
-      <input type="text" class="form-control" placeholder="Parool">
+      <input v-model="password" type="text" class="form-control" placeholder="Parool">
     </div>
 
+    <button @click=login type="submit" class="btn btn-outline-primary">Logi sisse</button>
 
   </div>
+
 </template>
 
 
 <script>
 
-export default {
-  name: 'ErrorView'
-}
+
 
 </script>
 
