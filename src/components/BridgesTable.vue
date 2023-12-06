fe2a9ae339cc3aaafc9f2cb850344971016144e7
Artjom Melnikov <artjom_melnikov@hotmail.com>
2023-12-06 12:12:47 +0200

    MAAS-4 wip

---
+++ src/components/BridgesTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Silla nr</th>
+        <th scope="col">Nimi</th>
+        <th scope="col">Pikkus [m]</th>
+        <th scope="col">Laius [m]</th>
+        <th scope="col">Seisundiindeks</th>
+        <th scope="col">Maakond</th>
+        <th scope="col">Materjal</th>
+      </tr>
+      </thead>
+      <tbody class="table-group-divider">
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'BridgesTable',
+
+  data() {
+    return {
+      bridgeId: "0"
+    }
+
+  },
+}
+</script>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AllBridgesView from '../views/AllBridgesView.vue'
 import LoginView from "@/views/LoginView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
+
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },{
     path: '/login',
     name: 'loginRoute',
     component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    path: '/allbridges',
+    name: 'allbridgesRoute',
+    component: AllBridgesView
+
+  },
+  {
+    path: '/home',
+    name: 'homeRoute',
+    component: HomeView
+
   }
 ]
 
