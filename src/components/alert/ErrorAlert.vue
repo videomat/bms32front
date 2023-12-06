993b47cf1209eff014ce5f75f470ee43b6a5161a
Martin <martin.koovit@hotmail.com>
2023-12-06 14:14:02 +0200

    MAAS4 Complete

---
+++ src/components/alert/ErrorAlert.vue
@@ -0,0 +1,15 @@
+<template>
+<div v-if="errorMessage.length > 0" class="alert alert-danger" role="alert">
+  {{errorMessage}}
+</div>
+</template>
+
+<script>
+export default{
+
+  name:'ErrorAlert',
+  props:{
+    errorMessage: String
+  }
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="container text-center background-image">
-
-      <div class="row justify-content-center row-position">
-
-        <div class="col col-2">
-          <div class="col mb-2">
-            <input v-model="username" type="text" class="form-control" placeholder="Kasutaja">
-          </div>
-
-          <div class="col mb-2">
-            <input v-model="password" type="text" class="form-control" placeholder="Parool">
-          </div>
-          <button @click=login type="submit" class="btn btn-outline-primary">Logi sisse</button>
+    <div class="row justify-content-center row-position">
+      <ErrorAlert :error-message="errorMessage"/>
+      <div class="col col-2">
+        <div class="col mb-2">
+          <input v-model="username" class="form-control" placeholder="Kasutaja" type="text">
         </div>
 
+        <div class="col mb-2">
+          <input v-model="password" class="form-control" placeholder="Parool" type="text">
+        </div>
+        <button class="btn btn-outline-primary" type="submit" @click="login">Logi sisse</button>
       </div>
-
-
-
+    </div>
   </div>
 
 
@@ -25,7 +20,77 @@
 
 
 <script>
+import router from "@/router";
+import ErrorAlert from "@/components/alert/ErrorAlert.vue";
 
+export default {
+  name: "Loginview",
+  components: {ErrorAlert},
+  data() {
+    return {
+      errorMessage: '',
+      username: '',
+      password: '',
+      loginResponse: {
+        userId: 0,
+        roleName: '',
+        errorResponse: {
+          message: '',
+          errorCode: 0
+        }
+      }
+    }
+  },
+  methods: {
+
+    login() {
+      if (this.allRequiredFieldsAreFilled()) {
+        this.sendLoginRequest();
+      } else {
+        this.handleRequiredFieldsAlert();
+      }
+    },
+    allRequiredFieldsAreFilled() {
+      return this.username.length > 0 && this.password > 0
+    },
+
+    sendLoginRequest() {
+      this.$http.get("/login", {
+        params: {
+          username: this.username,
+          password: this.password
+        },
+      }).then(response => {
+        this.loginResponse = response.data
+        this.handleSuccessfulLogin();
+      }).catch(error => {
+        this.handleUnsuccessfulLogin(error);
+      })
+    },
+    handleSuccessfulLogin() {
+      sessionStorage.setItem('userId', this.loginResponse.userId)
+      sessionStorage.setItem('roleName', this.loginResponse.roleName)
+      router.push({name: 'home'})
+    },
+    handleUnsuccessfulLogin(error) {
+      this.errorResponse = error.response.data
+      const httpStatusCode = error.response.status
+      if (httpStatusCode === 403 && this.errorResponse.errorCode === 111) {
+        this.errorMessage = this.errorResponse.message
+        setTimeout(this.resetErrorMessage, 4000)
+      } else {
+        router.push({name: 'errorRoute'})
+      }
+    },
+    handleRequiredFieldsAlert() {
+      this.errorMessage = 'Täida kõik väljad!'
+      setTimeout(this.resetErrorMessage, 5000)
+    },
+    resetErrorMessage() {
+      this.errorMessage = ''
+    }
+  }
+}
 
 </script>
 
